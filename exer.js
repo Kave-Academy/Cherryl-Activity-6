1b378e8ad1c8f293764f4b65e98f28e7159b7d32
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-18 15:44:47 +0800

    Finalize the border and the answer for the activity 6

--- exer.js
+++ exer.js
@@ -1,2 +1,26 @@
 function run() {
+
+    let firstName = document.getElementById("input_fname").value;
+    let lastName = document.getElementById("input_lname").value;
+
+        //get the mytable id
+        var table = document.getElementById("myTable");
+
+        //create an empty <tr> abd add it to the 2nd position
+        // since we have inputed <tr> on our HTML
+        var row = table.insertRow(1);
+
+        // Insert new cells (<td> elements) at the 1st and 
+        //2nd position of the "new" <tr> element:
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+
+        //add the value inputed at FirstName and LastName
+        cell1.innerHTML = firstName;
+        cell2.innerHTML = lastName;
+
+
 }
+
+
+
